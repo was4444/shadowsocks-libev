0128ddfc60ac095d4ab2e3a935e78a577fbcb56d
cuckoo <ur.cuckoo@gmail.com>
2015-10-11 09:57:42 +0800

    update genrpm.sh
    * use latest version by default;
    * use `shadowsocks-libev.spec.template' to generate the final
      `shadowsocks-libev.spec', to avoid disturbing git;

--- rpm/genrpm.sh
+++ rpm/genrpm.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
+
+name="shadowsocks-libev"
+spec_name="${name}.spec"
+version=$(git tag -l v* | tail -1)
+format="tar.xz"
+repo_dir=$(pushd $(dirname $(readlink -e $0)) > /dev/null && git rev-parse --show-toplevel)
+
+
 show_help()
 {
-    echo -e "`basename $0`  [option] [argument]"
+    echo -e "Usage:"
+    echo -e "  `basename $0`  [option] [argument]"
     echo
     echo -e "Options:"
     echo -e "  -h    show this help."
-    echo -e "  -v    with argument version (2.4.0 by default)."
-    echo -e "  -f    with argument format (tar.xz by default) used by git archive."
+    echo -e "  -v    with argument version (\`${version}' by default)."
+    echo -e "  -f    with argument format (\`${format}' by default) used by \`git archive'."
+    echo
+    echo -e "Description:"
+    echo -e "  This will use \`git archive' to generate a spefical version of "
+    echo -e "  \`${name}', which then will be used to build the final rpm packages."
     echo
     echo -e "Examples:"
-    echo -e "  to build base on version \`2.4.0' with format \`tar.xz', run:"
-    echo -e "    `basename $0` -f tar.xz -v 2.4.0"
+    echo -e "  to build base on version \`${version}' with format \`${format}', run:"
+    echo -e "    `basename $0` -f tar.xz -v ${version}"
 }
 
 while getopts "hv:f:" opt
@@ -23,43 +36,28 @@ do
             exit 0
             ;;
         v)
-            if [ "${OPTARG}" = v* ]; then
-                version=${OPTARG#"v"}
-            else
-                version=${OPTARG}
-            fi
+            version=${OPTARG}
             ;;
         f)
             format=${OPTARG}
             ;;
         *)
+            echo "try \``basename $0` -h' for more details."
             exit 1
             ;;
     esac
 done
 
-get_att_val()
-{
-    att=$1
-    val=$2
+version=${version#"v"}
 
-    if [ -z $(eval echo \$$att) ]; then
-        eval $att=$val
-    fi
-}
-
-get_att_val version "2.4.0"
-get_att_val format "tar.xz"
-
-name="shadowsocks-libev"
-spec_name="shadowsocks-libev.spec"
-
-pushd `git rev-parse --show-toplevel`
-git archive v${version} --format=${format} --prefix=${name}-${version}/ -o rpm/SOURCES/${name}-${version}.${format}
+pushd ${repo_dir}
+git archive v${version} \
+    --format=${format} --prefix=${name}-${version}/ \
+    -o rpm/SOURCES/${name}-${version}.${format}
 pushd rpm
 
-sed -i -e "s/^\(Version:        \).*$/\1${version}/" \
-       -e "s/^\(Source0:        %{name}-%{version}\.\).*$/\1${format}/" \
-    SPECS/${spec_name}
+sed -e "s/__FAKE_VERSION_TO_BE_SUBSTITUTED__/${version}/g" \
+    -e "s/__FAKE_FORMAT_TO_BE_SUBSTITUTED__/${format}/g" \
+    SPECS/${spec_name}.template > SPECS/${spec_name}
 
 rpmbuild -bb SPECS/${spec_name} --define "%_topdir `pwd`"
